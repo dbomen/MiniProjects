b8ce9f37fce54763369348fc45e419746478be62
dbomen <domen9.breznik@gmail.com>
2024-03-28 20:26:36 +0100

    C 5kyu

--- codewars/c/5kyu/incrementString.c
+++ codewars/c/5kyu/incrementString.c
@@ -1,47 +1,60 @@
-
-
 #include <stdbool.h>
 #include <stdlib.h>
-
-int numOfDigits(int n);
+#include <string.h>
 
 char* incrementString(const char* str) {
 
     int size = strlen(str);
-    bool numStarting = false;
-    int num = 0;
+    int sizeOfNum = size;
 
-    char* newStr = calloc(size, sizeof(int));
-    for (int i = 0; i < strlen(str); i++) {
+    for (int i = size - 1; i >= 0; i--) {
         char value = str[i];
 
-        if (value >= '0' && value <= '9')  numStarting = true;
-
-        if (numStarting) {
-            num *= 10;
-            num += value - '0';
-        } else {
-            newStr[i] = value;
+        if (!(value >= '0' && value <= '9')) {
+            sizeOfNum = size - i - 1;
+            break;
         }
     }
 
-    num++;
-    int numSize = numOfDigits(num);
-    for (int i = size - numSize; i < size; i++) {
-        newStr[i] = num % 10 * numSize;
-        numSize--;
+    char* num = calloc(sizeOfNum, sizeof(char));
+    for (int i = 0; i < sizeOfNum; i++)  num[i] = str[size - sizeOfNum + i];
+
+    int newSizeOfNum = sizeOfNum;
+    if (sizeOfNum != 0) {
+
+        bool changed = false;
+        int cntOf9 = 0;
+        for (int i = sizeOfNum - 1; i >= 0; i--) {
+            if (num[i] != '9') {
+                num[i]++;
+                changed = true;
+                break;
+            }
+            cntOf9++;
+        }   
+
+        // if the number is for e.g 999 we have to make another arr
+        if (!changed) {
+            char* newNum = calloc(sizeOfNum + 1, sizeof(char));
+            newNum[0] = '1';
+            for (int i = 0; i < sizeOfNum; i++)  newNum[i + 1] = '0';
+
+            num = newNum;
+            newSizeOfNum++;   
+        } else if (cntOf9 > 0) {
+            for (int i = sizeOfNum - 1; cntOf9 > 0; i--) {
+                num[i] = '0';
+                cntOf9--;
+            }
+        }
     }
 
+    bool noNumber = (newSizeOfNum == 0);
+    char* newStr = calloc(size - sizeOfNum + newSizeOfNum + ((noNumber) ? 1 : 0), sizeof(char));
+    int cnt = 0;
+    for (; cnt < size - sizeOfNum; cnt++)          newStr[cnt] = str[cnt];
+    for (int i = 0; i < newSizeOfNum; i++, cnt++)  newStr[cnt] = num[i];
 
-    
-}
-
-int numOfDigits(int n) {
-    int size = 0;
-    while (n > 0) {
-        n /= 10;
-        size++;
-    }
-
-    return size;
+    if (noNumber)  newStr[cnt] = '1';
+    return newStr;
 }
